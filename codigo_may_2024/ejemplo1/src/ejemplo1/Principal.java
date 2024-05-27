d50af290d346cf229af29b38f7f8fdf1939dc29a
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-27 11:54:20 +0200

    11.54

---
+++ codigo_may_2024/ejemplo1/src/ejemplo1/Principal.java
@@ -0,0 +1,9 @@
+package ejemplo1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		System.out.println("ejemplo de prueba");
+	}
+
+}
--- codigo_may_2024/operaciones/src/es/curso/operaciones/Principal.java
+++ codigo_may_2024/operaciones/src/es/curso/operaciones/Principal.java
@@ -3,44 +3,94 @@ package es.curso.operaciones;
 public class Principal {
 
 	public static void main(String[] args) {
-		
+
 		pruebaOperadores();
-		calcularEcuGrado2();
+		//calcularEcuGrado2();
+		
+		
 	}
 
 	private static void calcularEcuGrado2() {
 		// TODO Auto-generated method stub
-		
+
+		int a, b, c; // 1,2,3 no tiene solución
+		double x1, x2, raiz;
+
 		// Potencia:
 		System.out.println(Math.pow(2, 3));
+
+		// Raíz
 		System.out.println(Math.sqrt(25));
-		
-		int a,b,c; // 1,2,3 no tiene solución
-		
+
 		a = 1;
-		b = 5; 
-		c = 4;
-		
-		double x1, x2;
-		
-		//x1 = -b +
-		//x2 = -b - 
-		
+		b = 2;
+		c = 3;
+
+		raiz = Math.pow(b, 2.0) - 4 * a * c;
+
+		if (raiz > 0) {
+			x1 = (-b + Math.sqrt(raiz)) / (2 * a);
+			x2 = (-b - Math.sqrt(raiz)) / (2 * a);
+
+			System.out.println("x1: " + x1 + " x2: " + x2);
+			
+		} else {
+			System.out.println("No hay solución");
+		}
+
 	}
 
 	private static void pruebaOperadores() {
 		// TODO Auto-generated method stub
-		
+
 		int a, b;
-		
+
 		a = 10;
 		b = ++a;
-		System.out.println("a: "+a +" b:"+b);
-		
+		System.out.println("a: " + a + " b:" + b);
+
 		a = 10;
 		b = a++;
-		System.out.println("a: "+a +" b:"+b);
+		System.out.println("a: " + a + " b:" + b);
+		
+		// Calcular el iva de un importe:
+		double importe = 1200.56;
+		double iva;
+		
+		iva = importe * 0.21;
+		System.out.println("importe: "+importe + " iva: " + iva);
 		
 		
+		// DEfinición de letras o char:
+		char letra = 'A';
+		letra++;
+		System.out.println("Letra: "+ letra + " numero: " + (int) letra);
+		
+		letra = (char) (letra + 5);
+		
+		// Obtener la parte entera de un número real:
+		double horas = 2.5; // h = 2, m = 30
+		double minutos;
+		int h, m;
+		
+		h = (int) horas;
+		minutos = horas - h;
+		m = (int) (minutos * 60.0);
+		System.out.println("h: " + h + " m:" + m );
+		
+		// Convertir h y m a double:
+		double horas2 = h + (m / 60.0);
+		System.out.println("horas2: "+horas2);
+		
+		double horas3 = h + (m / 60);
+		System.out.println("horas3: "+horas3);
+		
+		// Convertir un numero a char:
+		int num = 67;
+		letra = (char) num;
+		System.out.println("Letra: " + letra);
+		
+		
+				
 	}
 }
