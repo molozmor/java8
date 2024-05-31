bd2fd1d6c10815df673298ba9ba5ff4e16f04cce
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-31 13:03:17 +0200

    13.03

--- codigo_may_2024/colecciones/src/colecciones/Principal.java
+++ codigo_may_2024/colecciones/src/colecciones/Principal.java
@@ -2,16 +2,62 @@ package colecciones;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		pruebaListas();
+		//pruebaListas();
+		pruebaMapas();
+	}
+
+	private static void pruebaMapas() {
+		// Definir, crear, añadir claves, recuperar valores, recorridos, ...
+		
+		// Ni ordena por clave y tampoco mantiene el orden de inserción
+		Map<String, Integer> gastos = new HashMap<String, Integer>();
+		gastos.put("Lunes", 230);
+		gastos.put("Viernes", 12);
+		gastos.put("Jueves", 145);
+		gastos.put("Sabado", 500);
+		
+		System.out.println(gastos);
+				
+		
+		for (String key : gastos.keySet()) {
+			System.out.println(key + " " + gastos.get(key) + " " + key.hashCode());
+		}
+		
+		gastos.put("Viernes", 222);
+		System.out.println(gastos);
+		
+		// Con el orden de inserción:
+		Map<String, Integer> gastos2 = new LinkedHashMap<String, Integer>();
+		gastos2.put("Lunes", 230);
+		gastos2.put("Viernes", 12);
+		gastos2.put("Jueves", 145);
+		gastos2.put("Sabado", 500);
+	
+		System.out.println(gastos2);
+		
+		// Mantiendo un orden personalizado:
+		Map<String, Integer> gastos3 = new TreeMap<String, Integer>();
+		gastos3.put("Lunes", 230);
+		gastos3.put("Viernes", 12);
+		gastos3.put("Jueves", 145);
+		gastos3.put("Sabado", 500);
+	
+		System.out.println(gastos3);
+		
+		
 	}
 
 	private static void pruebaListas() {
