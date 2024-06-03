54cf7a2d1c2fe538facd6df55a66fbf28e49ba99
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-03 14:17:21 +0200

    14.17

--- codigo_may_2024/serializacion/src/serializacion/Principal.java
+++ codigo_may_2024/serializacion/src/serializacion/Principal.java
@@ -1,7 +1,16 @@
 package serializacion;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 public class Principal {
 	
+	private static final String PATH_FICHERO = "ficheros/persona.dat";
+
 	public static void main(String[] args) {
 		
 		Persona persona = new Persona("Jose Miguel", 34, 1.8, true);
@@ -17,12 +26,69 @@ public class Principal {
 	}
 
 	private Persona deserializar() {
-		// TODO Auto-generated method stub
-		return null;
+		FileInputStream fichero = null;
+		ObjectInputStream obj = null;
+		Persona persona = null;
+		
+		try {
+			fichero = new FileInputStream(PATH_FICHERO);
+			obj = new ObjectInputStream(fichero);
+			persona = (Persona) obj.readObject();		
+			
+		} catch (IOException | ClassNotFoundException e) {
+			System.out.println(e.getMessage());
+			
+		} finally {
+			if (obj != null) {
+				try {
+					obj.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			if (fichero != null) {
+				try {
+					fichero.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		return persona;
 	}
 
 	private void serializar(Persona persona) {
-		// TODO Auto-generated method stub
+		FileOutputStream fichero = null;
+		ObjectOutputStream obj = null;
+		
+		try {
+			fichero = new FileOutputStream(PATH_FICHERO);
+			obj = new ObjectOutputStream(fichero);
+			obj.writeObject(persona);		
+			
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+			
+		} finally {
+			if (obj != null) {
+				try {
+					obj.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			if (fichero != null) {
+				try {
+					fichero.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
 		
 	}
 
