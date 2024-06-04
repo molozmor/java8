960f494007ec72e95133768197d46acaa39b1ab4
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-04 09:42:41 +0200

    9.42

--- codigo_may_2024/ficheros_properties/src/ficheros_properties/Principal.java
+++ codigo_may_2024/ficheros_properties/src/ficheros_properties/Principal.java
@@ -2,18 +2,33 @@ package ficheros_properties;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Iterator;
 import java.util.Properties;
+import java.util.Set;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		Properties properties;
 		
+		
 		properties = new Properties();
 		try {
 			properties.load(new FileInputStream("src/config.properties"));
 			System.out.println(properties.getProperty("path_origen"));
 			System.out.println(properties.getProperty("path_destino"));
+			
+			// Cargar propiedades del sistema:
+			properties = System.getProperties();
+			Enumeration<Object> claves = properties.keys();
+			String clave;
+						
+			while (claves.hasMoreElements()) {
+				clave = (String) claves.nextElement();
+				System.out.println(clave + " ==> " + properties.get(clave));
+			}
+			
 			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
