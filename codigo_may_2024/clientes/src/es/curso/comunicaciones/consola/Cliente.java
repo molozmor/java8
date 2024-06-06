309a4d0be99eb2a4d535987d8176f67e95826449
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-06 10:29:20 +0200

    10.29

--- codigo_may_2024/clientes/src/es/curso/comunicaciones/consola/Cliente.java
+++ codigo_may_2024/clientes/src/es/curso/comunicaciones/consola/Cliente.java
@@ -4,6 +4,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Cliente {
 	
@@ -11,12 +12,14 @@ public class Cliente {
 	public static final int PUERTO = 4545;
 
 	public static void main(String[] args) {
-		Socket cliente;
+		Socket cliente = null;
 		
 		// Para leer / escri
-		DataInputStream canalEntrada;
-		DataOutputStream canalSalida;
+		DataInputStream canalEntrada = null;
+		DataOutputStream canalSalida = null;
 			
+		String texto, respuesta;
+		Scanner scanner;
 		
 		try {
 			// Crear el socket:
@@ -26,15 +29,41 @@ public class Cliente {
 			canalEntrada = new DataInputStream(cliente.getInputStream());
 			canalSalida = new DataOutputStream(cliente.getOutputStream());
 			
+			// Crear el scanner para leer de teclado:
+			scanner = new Scanner(System.in);
 			
+			do {
+				// Leer de teclado:
+				System.out.print("Mensaje:> ");
+				texto = scanner.nextLine();
+				
+				// Enviar al servidor:
+				canalSalida.writeUTF(texto);
+				
+				// Esperar la respuesta del servidor:
+				respuesta = canalEntrada.readUTF();
+				
+				System.out.println("SERVER: "+respuesta);
+				
+			} while (texto.equalsIgnoreCase("fin"));
 			
 			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println(e.getMessage());
+			
+		} finally {
+			
+				try {
+					if (canalEntrada != null) canalEntrada.close();
+					if (canalSalida != null) canalSalida.close();
+					if (cliente!= null) cliente.close();
+					
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}			
 		}
 		
-
 	}
 
 }
