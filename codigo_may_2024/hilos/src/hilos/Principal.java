ade45053b076f12d6904bf31abf02fc4c9fe8df3
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-07 10:12:12 +0200

    10.12

--- codigo_may_2024/hilos/src/hilos/Principal.java
+++ codigo_may_2024/hilos/src/hilos/Principal.java
@@ -1,11 +1,15 @@
 package hilos;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		
-		lanzar2Hilos();
-		//coleccionHilos();
+		//lanzar2Hilos();
+		coleccionHilos();
 
 	}
 
@@ -17,7 +21,7 @@ public class Principal {
 			h1 = new Mensajes(10, "H1");
 			h2 = new Mensajes(8, "H2");
 			
-			h1.start();				
+			h1.start();					
 			h2.start();
 			
 			h1.join();
@@ -33,8 +37,26 @@ public class Principal {
 	}
 
 	private static void coleccionHilos() {
-		// TODO Auto-generated method stub
+		List<Mensajes> hilos;
+		Mensajes hilo;
+		Random random = new Random();
 		
+		hilos = new ArrayList<Mensajes>();
+		for (int i = 0 ; i < 4 ; i++) {
+			hilo = new Mensajes(random.nextInt() % 15, "H-"+(i+1));
+			hilo.start();
+			hilos.add(hilo);
+			//hilo.join(); ojo secualizamos el programa!
+		}
+		for (Mensajes h : hilos) {
+			try {
+				h.join();
+			} catch (InterruptedException e) {				
+				e.printStackTrace();
+			}
+		}
+		System.out.println("Hilo actual: "+Thread.currentThread().getName());
+		System.out.println("Hilo activos: "+Thread.activeCount());
 	}
 
 }
