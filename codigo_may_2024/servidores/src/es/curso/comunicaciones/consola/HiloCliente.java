431195626e570584be310eeb3274172e1ce93959
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-07 09:53:09 +0200

    9.56

--- codigo_may_2024/servidores/src/es/curso/comunicaciones/consola/HiloCliente.java
+++ codigo_may_2024/servidores/src/es/curso/comunicaciones/consola/HiloCliente.java
@@ -1,17 +1,59 @@
 package es.curso.comunicaciones.consola;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
 
 public class HiloCliente extends Thread {
 
-	public HiloCliente(Socket cliente) {
+	private Socket cliente;
+	private DataInputStream canalEntrada;
+	private DataOutputStream canalSalida;
+
+	public HiloCliente(Socket cliente) throws IOException {
 		// TODO Auto-generated constructor stub
+		this.cliente = cliente;
+
+		// Habilitar canales de E/S:
+		canalEntrada = new DataInputStream(cliente.getInputStream());
+		canalSalida = new DataOutputStream(cliente.getOutputStream());
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		String texto;
 
+		try {
+			do {
+				// Esperar el mensaje del cliente:
+				texto = canalEntrada.readUTF();
+				System.out.println("CLIENTE: " + texto);
+
+				// Enviar respuesta al cliente:
+				canalSalida.writeUTF(texto.toUpperCase());
+
+			} while (!texto.equalsIgnoreCase("fin"));
+
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+			
+		} finally {
+			try {
+				if (canalEntrada != null)
+					canalEntrada.close();
+
+				if (canalSalida != null)
+					canalSalida.close();
+				
+				if (cliente != null)
+					cliente.close();
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
--- codigo_may_2024/servidores/src/es/curso/comunicaciones/consola/ServidorMultiCliente.java
+++ codigo_may_2024/servidores/src/es/curso/comunicaciones/consola/ServidorMultiCliente.java
@@ -1,7 +1,5 @@
 package es.curso.comunicaciones.consola;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -14,12 +12,8 @@ public class ServidorMultiCliente {
 		// TODO Auto-generated method stub
 		ServerSocket server = null;
 		Socket cliente = null;
-		String texto;
 		HiloCliente hiloCliente;
-		
-		// Para leer / escribir
-		DataInputStream canalEntrada = null;
-		DataOutputStream canalSalida = null;
+				
 		
 		try {
 			// Crear el socket para atender a los clientes:			
@@ -39,34 +33,14 @@ public class ServidorMultiCliente {
 				hiloCliente = new HiloCliente(cliente);
 				hiloCliente.start();
 			}
-			
-			
-			
-			
-			// Habilitar canales de E/S:
-			canalEntrada = new DataInputStream(cliente.getInputStream());
-			canalSalida = new DataOutputStream(cliente.getOutputStream());
-			
-			do {
-				// Esperar el mensaje del cliente:
-				texto = canalEntrada.readUTF();
-				
-				System.out.println("CLIENTE: "+texto);
-				
-				// Enviar respuesta al cliente:
-				canalSalida.writeUTF(texto.toUpperCase());
-				
-			} while (!texto.equalsIgnoreCase("fin"));
+	
 			
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
 			
 		}  finally {
 			
-			try {
-				if (canalEntrada != null) canalEntrada.close();
-				if (canalSalida != null) canalSalida.close();
-				if (cliente!= null) cliente.close();
+			try {			
 				if (server != null) server.close();
 				
 			} catch (IOException e) {
