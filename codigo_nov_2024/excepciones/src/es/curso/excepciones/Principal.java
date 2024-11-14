4a8667db1a885cefc8551020d6f798b142bfb914
aldebarran22 <antonioespinherranz@hotmail.com>
2024-11-14 12:41:16 +0100

    12.41

---
+++ codigo_nov_2024/excepciones/src/es/curso/excepciones/Principal.java
@@ -0,0 +1,28 @@
+package es.curso.excepciones;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		pruebas();
+		
+	}
+	
+	
+
+	private static void pruebas() {
+		// TODO Auto-generated method stub
+		prueba1(10);
+	}
+
+
+
+	private static void prueba1(int i) {
+		int[] numeros = {1,2,3,4,5};
+		
+		System.out.println(numeros[i]);
+		
+	}
+
+}
