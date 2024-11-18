27f4d9225d34ba5016492f8aa15e8acfabfe5383
aldebarran22 <antonioespinherranz@hotmail.com>
2024-11-18 13:24:14 +0100

    13.24

--- codigo_nov_2024/ficheros/src/es/curso/ficheros/Principal.java
+++ codigo_nov_2024/ficheros/src/es/curso/ficheros/Principal.java
@@ -1,18 +1,22 @@
 package es.curso.ficheros;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//leerFichero("src/es/curso/datos/Empleados.txt");
-		grabarFichero("ficheros/resultados/texto.txt");
+		// leerFichero("src/es/curso/datos/Empleados.txt");
+		// grabarFichero("ficheros/resultados/texto.txt");
+		// leerFicheroPorFilas("ficheros/pedidos.csv");
+		generarPedidosPais("ficheros/pedidos.csv");
 	}
 
 	private static void grabarFichero(String path) {
@@ -23,11 +27,11 @@ public class Principal {
 
 		try {
 			// Modo w: write, si el fichero existe lo machaca
-			//fichero = new FileOutputStream(path);
-			
+			fichero = new FileOutputStream(path);
+
 			// Modo a: append, añadir contenido al final del fichero
-			fichero = new FileOutputStream(path, true);
-			
+			// fichero = new FileOutputStream(path, true);
+
 			for (String mes : meses) {
 				fichero.write(mes.getBytes());
 				fichero.write("\n".getBytes());
@@ -47,6 +51,98 @@ public class Principal {
 				}
 			}
 		}
+	}
+
+	private static void leerFicheroPorFilas(String path) {
+		Scanner scan = null;
+
+		try {
+			scan = new Scanner(new File(path));
+
+			// Lectura del fichero: byte a byte!
+			while (scan.hasNextLine()) {
+				System.out.println(scan.nextLine());
+			}
+
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+
+		} finally {
+
+			if (scan != null) {
+				scan.close();
+			}
+		}
+
+	}
+
+	private static void generarPedidosPais(String path) {
+		Scanner scan = null;
+		String linea, pais;
+		String[] campos;
+		String pathDestino;
+		FileOutputStream ficheroPais;
+		String cabeceras = null;
+		File file;
+
+		try {
+			scan = new Scanner(new File(path));
+
+			// Lectura del fichero: byte a byte!
+			while (scan.hasNextLine()) {
+				linea = scan.nextLine();
+				if (cabeceras == null) {
+					cabeceras = linea;
+					continue;
+				}
+
+				campos = linea.split(";");
+				pais = campos[campos.length - 1].replace(' ', '_');
+				pathDestino = "ficheros/resultados/" + pais + ".csv";
+
+				// Comprobar si el fichero destino ya existía:
+				ficheroPais = null;
+				try {
+					
+					file = new File(pathDestino);
+					if (!file.isFile()) {
+						// Es la primera vez que encontramos ese pais
+						// Abrir el fichero y escribir las cabeceras:
+						ficheroPais = new FileOutputStream(file, true);
+						ficheroPais.write(cabeceras.getBytes());
+						ficheroPais.write("\n".getBytes());
+
+					} else {
+						// Si ya existe solo se abre:
+						ficheroPais = new FileOutputStream(file, true);
+					}
+
+					// Grabar el pedido:
+					ficheroPais.write(linea.getBytes());
+					ficheroPais.write("\n".getBytes());
+					
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+					
+				} finally {
+					if (ficheroPais != null)
+						ficheroPais.close();
+				}
+
+			}
+
+			// System.out.println(cabeceras);
+
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+
+		} finally {
+
+			if (scan != null) {
+				scan.close();
+			}
+		}
+
 	}
 
 	private static void leerFichero(String path) {
