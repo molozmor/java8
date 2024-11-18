bb430a9daeb34b2ffce7d6141992d97599f6e9b4
aldebarran22 <antonioespinherranz@hotmail.com>
2024-11-18 14:05:24 +0100

    14.05

---
+++ codigo_nov_2024/ficheros/src/es/curso/ficheros/GestorPedidos.java
@@ -0,0 +1,128 @@
+package es.curso.ficheros;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class GestorPedidos {
+
+	private Map<String, List<String>> mapaPaises;
+	private String path;
+	private String cabeceras;
+
+	public GestorPedidos(String path) {
+		this.path = path;
+		this.mapaPaises = new TreeMap<String, List<String>>();
+		this.cabeceras = null;
+
+		this.cargarMapa();
+	}
+
+	public void generarFicheros(String carpeta) {
+		FileOutputStream fichero;
+		String pathDestino;
+
+		for (String pais : this.mapaPaises.keySet()) {
+			fichero = null;
+			
+			try {
+				pathDestino = carpeta + pais + ".csv"; 
+				fichero = new FileOutputStream(pathDestino);
+				
+				// Grabar cabeceras:
+				fichero.write(cabeceras.getBytes());
+				fichero.write("\n".getBytes());
+				
+				for (String pedido : this.mapaPaises.get(pais)) {
+					fichero.write(pedido.getBytes());
+					fichero.write("\n".getBytes());
+				}
+
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+				
+			} finally {
+				if (fichero != null) {
+					try {
+						fichero.close();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+
+	}
+
+	public List<String> getNombrePaises() {
+		List<String> paises;
+
+		paises = new ArrayList<String>();
+		paises.addAll(mapaPaises.keySet());
+		return paises;
+	}
+
+	public int getNumPaises() {
+		return this.mapaPaises.size();
+	}
+
+	public int getNumPedidosPais(String pais) {
+		return this.mapaPaises.get(pais).size();
+	}
+
+	private void cargarMapa() {
+		Scanner scan = null;
+		String linea, pais;
+		String[] campos;
+		List<String> pedidos;
+
+		try {
+			scan = new Scanner(new File(path));
+
+			// Lectura del fichero: byte a byte!
+			while (scan.hasNextLine()) {
+				linea = scan.nextLine();
+				if (cabeceras == null) {
+					cabeceras = linea;
+					continue;
+				}
+
+				campos = linea.split(";");
+				pais = campos[campos.length - 1].replace(' ', '_');
+
+				// Colocar el pais en el mapa y añadir el pedido a su colección;
+				if (mapaPaises.containsKey(pais)) {
+					// Recuperamos la lista de pedidos de ese pais:
+					pedidos = mapaPaises.get(pais);
+
+				} else {
+					// Es la primera vez, hay que crear la colección
+					pedidos = new ArrayList<String>();
+				}
+
+				// Añadir el pedido a la colección:
+				pedidos.add(linea);
+
+				// Modificar el mapa:
+				mapaPaises.put(pais, pedidos);
+			}
+
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+
+		} finally {
+
+			if (scan != null) {
+				scan.close();
+			}
+		}
+
+	}
+
+}
--- codigo_nov_2024/ficheros/src/es/curso/ficheros/Principal.java
+++ codigo_nov_2024/ficheros/src/es/curso/ficheros/Principal.java
@@ -4,9 +4,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Principal {
 
@@ -16,7 +19,18 @@ public class Principal {
 		// leerFichero("src/es/curso/datos/Empleados.txt");
 		// grabarFichero("ficheros/resultados/texto.txt");
 		// leerFicheroPorFilas("ficheros/pedidos.csv");
-		generarPedidosPais("ficheros/pedidos.csv");
+		// generarPedidosPais("ficheros/pedidos.csv");
+		// generarPedidosPaisMapa("ficheros/pedidos.csv");
+		
+		pruebaGestorPedidos("ficheros/pedidos.csv");
+	}
+
+	private static void pruebaGestorPedidos(String path) {
+		// TODO Auto-generated method stub
+		
+		GestorPedidos gestorPedidos = new GestorPedidos(path);
+		System.out.println(gestorPedidos.getNombrePaises());
+		System.out.println("Alemania:"+gestorPedidos.getNumPedidosPais("Alemania"));
 	}
 
 	private static void grabarFichero(String path) {
@@ -75,6 +89,62 @@ public class Principal {
 		}
 
 	}
+	
+	private static void generarPedidosPaisMapa(String path) {
+		Scanner scan = null;
+		String linea, pais;
+		String[] campos;
+		String pathDestino;
+		FileOutputStream ficheroPais;
+		String cabeceras = null;
+		Map<String, List<String>> mapaPaises;
+		List<String> pedidos;
+
+		try {
+			mapaPaises = new TreeMap<String, List<String>>();
+			scan = new Scanner(new File(path));
+
+			// Lectura del fichero: byte a byte!
+			while (scan.hasNextLine()) {
+				linea = scan.nextLine();
+				if (cabeceras == null) {
+					cabeceras = linea;
+					continue;
+				}
+				
+				campos = linea.split(";");
+				pais = campos[campos.length - 1].replace(' ', '_');
+				
+				// Colocar el pais en el mapa y añadir el pedido a su colección;
+				if (mapaPaises.containsKey(pais)) {
+					// Recuperamos la lista de pedidos de ese pais:
+					pedidos = mapaPaises.get(pais);
+					
+				} else {
+					// Es la primera vez, hay que crear la colección
+					pedidos = new ArrayList<String>();
+				}
+				
+				// Añadir el pedido a la colección:
+				pedidos.add(linea);
+				
+				// Modificar el mapa:
+				mapaPaises.put(pais, pedidos);				
+			}	
+			
+			System.out.println();
+			
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+
+		} finally {
+
+			if (scan != null) {
+				scan.close();
+			}
+		}
+	}
+			
 
 	private static void generarPedidosPais(String path) {
 		Scanner scan = null;
