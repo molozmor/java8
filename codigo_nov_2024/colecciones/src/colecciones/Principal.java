b7d02c899c4b630355ec9773abcf1f984179846a
aldebarran22 <antonioespinherranz@hotmail.com>
2024-11-18 10:42:05 +0100

    10.41

--- codigo_nov_2024/colecciones/src/colecciones/Principal.java
+++ codigo_nov_2024/colecciones/src/colecciones/Principal.java
@@ -1,13 +1,11 @@
 package colecciones;
 
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -20,22 +18,23 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//pruebasListas();
-		//pruebaCalendarios();
-		//pruebaMapas();
-		pruebaListaPersonas();
+		// pruebasListas();
+		// pruebaCalendarios();
+		// pruebaMapas();
+		// pruebaListaPersonas();
+		histograma(50, 20);
 	}
 
 	private static void pruebaListaPersonas() {
 		// TODO Auto-generated method stub
 		List<Persona> personal;
-		
+
 		personal = new ArrayList<Persona>();
 		personal.add(new Persona("José", 30, 1.80, 80));
 		personal.add(new Persona("Ana", 34, 1.70, 70));
 		personal.add(new Persona("Miguel", 40, 1.60, 65));
 		personal.add(new Persona("Eva", 32, 1.78, 77));
-		
+
 		for (Persona p : personal) {
 			System.out.println(p);
 		}
@@ -43,79 +42,123 @@ public class Principal {
 
 	private static void pruebaMapas() {
 		// TODO Auto-generated method stub
-		
-		//Map<String, Integer> mapa = new HashMap<String, Integer>(); // No mantiene el orden de inserción
-		// Map<String, Integer> mapa = new LinkedHashMap<String, Integer>(); // Mantiene el orden
+
+		// Map<String, Integer> mapa = new HashMap<String, Integer>(); // No mantiene el
+		// orden de inserción
+		// Map<String, Integer> mapa = new LinkedHashMap<String, Integer>(); // Mantiene
+		// el orden
 		Map<String, Integer> mapa = new TreeMap<String, Integer>(); // Ordenar las claves
-		
+
 		mapa.put("uno", 1);
 		mapa.put("dos", 2);
 		mapa.put("cinco", 5);
 		mapa.put("tres", 3);
 		mapa.put("cuatro", 4);
-		
+
 		System.out.println(mapa);
-		
+
 		for (String key : mapa.keySet()) {
 			System.out.println(key + " => " + mapa.get(key));
 		}
-		
-		System.out.println("Contains key dos: "+mapa.containsKey("dos"));
-		System.out.println("Contains value 4: "+mapa.containsValue(4));
-		
-		
-		//Map<String, List<String[]>> mapa2;
-		
-		
-		// Utilizar un mapa para realizar un recuento de las veces que se repite un valor en una lista
+
+		System.out.println("Contains key dos: " + mapa.containsKey("dos"));
+		System.out.println("Contains value 4: " + mapa.containsValue(4));
+
+		// Map<String, List<String[]>> mapa2;
+
+		// Utilizar un mapa para realizar un recuento de las veces que se repite un
+		// valor en una lista
 		// Histograma
 		Random rd = new Random((new Date()).getTime());
 
 		// Elegimos una implementación
 		List<Integer> numeros = new ArrayList<>();
-		
-		for (int i = 0 ; i < 50 ; i++) {
+
+		for (int i = 0; i < 50; i++) {
 			numeros.add(rd.nextInt(30));
 		}
-		
+
 		Map<Integer, Integer> histograma = new HashMap<Integer, Integer>();
-		
-		
+
 	}
 
 	private static void pruebaCalendarios() {
 		// TODO Auto-generated method stub
 		List<Calendario> fechas = crearCalendarios(25);
-		
+
 		Collections.sort(fechas);
-		
+
 		for (Calendario c : fechas) {
 			System.out.println(c);
 		}
 	}
 
-	private static List<Calendario> crearCalendarios(int n){
-		List<String> meses = Arrays.asList("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");		
+	private static List<Calendario> crearCalendarios(int n) {
+		List<String> meses = Arrays.asList("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto",
+				"Septiembre", "Octubre", "Noviembre", "Diciembre");
 		Random rd;
 		List<Calendario> fechas = new ArrayList<Calendario>();
 		Calendario cal;
 		Date d = new Date();
 		int dia, mes;
-		
+
 		rd = new Random(d.getTime());
-		
-		for (int i = 0 ; i < n ; i++) {
+
+		for (int i = 0; i < n; i++) {
 			dia = 1 + rd.nextInt(30);
 			mes = rd.nextInt(12);
-			
+
 			cal = new Calendario(dia, meses.get(mes));
 			fechas.add(cal);
-		}	
-		
+		}
+
 		return fechas;
 	}
-	
-	
+
+	private static void histograma(int tam, int tope) {
+		// Generar tam números aleatorios, se almacenan en una lista
+		// Crear un histograma a partir de la lista: mapa
+		List<Integer> numeros;
+		Random rd;
+		Date d = new Date();		
+		Map<Integer, Integer> histo = new TreeMap<Integer, Integer>();
+		int valor;
+		
+		rd = new Random(d.getTime());
+
+		// Elegimos una implementación
+		numeros = new ArrayList<>();
+
+		for (int i = 0; i < tam; i++) {
+			numeros.add(rd.nextInt(tope));
+		}
+		
+		System.out.println(numeros);
+		
+		// Utilizar el mapa para crear un histograma. Cuantas veces se repite cada número.
+		// Manteniendo el orden de los números (claves) --> TreeMap
+		for (int numero : numeros) {
+			if (histo.containsKey(numero)) {
+				// Incrementar en uno el valor de la clave
+				valor = histo.get(numero) + 1;
+				
+			} else {
+				// Es la primera vez, se inicializa a 1 la clave:
+				valor = 1;
+			}
+			
+			histo.put(numero, valor);
+		}
+		
+		// Recorrer el mapa:
+		System.out.println("\n\nHISTOGRAMA:");
+		for (int clave : histo.keySet()) {
+			System.out.println("Número: "+clave+" se repite "+histo.get(clave)+" veces");
+		}
+		
+		
+	}
+
 	private static void pruebasListas() {
 		// List<int> numeros; // No podemos utilizar tipos primitivos
 		List<Integer> numeros;
@@ -123,51 +166,52 @@ public class Principal {
 		Date d = new Date();
 		int numero;
 		Iterator<Integer> it;
-		
+
 		rd = new Random(d.getTime());
 
 		// Elegimos una implementación
 		numeros = new ArrayList<>();
-		
-		for (int i = 0 ; i < 50 ; i++) {
+
+		for (int i = 0; i < 50; i++) {
 			numeros.add(rd.nextInt(30));
 		}
-		
+
 		System.out.println(numeros);
-		System.out.println(numeros.size()+" números");
-		
+		System.out.println(numeros.size() + " números");
+
 		// Búsquedas
 		numero = 23;
 		if (numeros.contains(numero)) {
-			System.out.println(numero + " existe en la pos: "+numeros.indexOf(numero)); // El primero de la izq.
-			System.out.println(numero + " existe en la pos última: "+numeros.lastIndexOf(numero)); // El último (por la derecha)
-			
+			System.out.println(numero + " existe en la pos: " + numeros.indexOf(numero)); // El primero de la izq.
+			System.out.println(numero + " existe en la pos última: " + numeros.lastIndexOf(numero)); // El último (por
+																										// la derecha)
+
 		} else {
-			System.out.println("No existe ..."); 
+			System.out.println("No existe ...");
 		}
-		
+
 		// Recorridos:
 		System.out.println("\nPrimer recorrido:");
-		for (int i = 0 ; i < numeros.size() ; i++) {
-			System.out.print(numeros.get(i)+" ");
+		for (int i = 0; i < numeros.size(); i++) {
+			System.out.print(numeros.get(i) + " ");
 		}
 		System.out.println("\nSegundo recorrido:");
 		for (int num : numeros) {
-			System.out.print(num+" ");
+			System.out.print(num + " ");
 		}
 		System.out.println("\nTercer recorrido:");
 		it = numeros.iterator();
 		while (it.hasNext()) {
-			System.out.print(it.next()+" ");
+			System.out.print(it.next() + " ");
 		}
 		System.out.println();
-		
+
 		// Ordenar la colección:
 		Collections.sort(numeros);
 		System.out.println(numeros);
-		
+
 		// Como inicializar listas a valores:
-		List<String> nombres = Arrays.asList("Ana","Eva","Pedro", "julián","Alberto");
+		List<String> nombres = Arrays.asList("Ana", "Eva", "Pedro", "julián", "Alberto");
 		Collections.sort(nombres);
 		System.out.println(nombres);
 	}
