2165261c1b2a1881eaa567a329404e98c6cc849e
aldebarran22 <antonioespinherranz@hotmail.com>
2024-11-19 13:05:54 +0100

    13.05

---
+++ codigo_nov_2024/base_datos/src/es/curso/basedatos/Principal.java
@@ -0,0 +1,85 @@
+package es.curso.basedatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class Principal {
+
+	private static final String URL_BD = "jdbc:mysql://localhost:3306/empresa3?serverTimezone=UTC";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ejecutarQuery();
+	}
+
+	private static void ejecutarQuery() {
+		// TODO Auto-generated method stub
+
+		Connection conexion = null;
+		Scanner scan;
+		String sql = null;
+		Statement st = null;
+		ResultSet rst = null;
+		ResultSetMetaData rstmt;
+		int cols;
+
+		try {
+			conexion = DriverManager.getConnection(URL_BD, "root", "antonio");
+			System.out.println("Conexión ok!");
+			st = conexion.createStatement();
+
+			scan = new Scanner(System.in);
+			do {
+
+				try {
+					System.out.print("SQL:> ");
+					sql = scan.nextLine();
+
+					// Ejecutar el SQL;
+					rst = st.executeQuery(sql);
+					rstmt = rst.getMetaData();
+					cols = rstmt.getColumnCount();
+
+					while (rst.next()) {
+
+						for (int i = 1; i <= cols; i++) {
+							System.out.print(rst.getString(i) + "\t");
+						}
+						System.out.println();
+					}
+					
+				} catch (Exception e) {
+					System.out.println("error: " + e.getMessage());
+				}
+
+			} while (!sql.equals(""));
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("error: " + e.getMessage());
+
+		} finally {
+
+			try {
+				if (rst != null)
+					rst.close();
+				if (st != null)
+					st.close();
+				if (conexion != null)
+					conexion.close();
+
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+	}
+
+}
