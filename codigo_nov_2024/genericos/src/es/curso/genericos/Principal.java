18d2e78ee3f9765e6e1f9c1bc62c8ca6af52b1bc
aldebarran22 <antonioespinherranz@hotmail.com>
2024-11-20 14:31:17 +0100

    1235

--- codigo_nov_2024/genericos/src/es/curso/genericos/Principal.java
+++ codigo_nov_2024/genericos/src/es/curso/genericos/Principal.java
@@ -1,14 +1,54 @@
 package es.curso.genericos;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Principal {
+	
+	
+	/*
+	public static void ordenar(int[] array) {
+		int aux;
+		
+		for (int i = 0 ; i < array.length-1 ; i++) {
+			for (int j = i+1 ; j < array.length ; j++) {
+				if (array[i] > array[j]) {
+					aux = array[i];
+					array[i] = array[j];
+					array[j] = aux;
+				}
+			}
+		}
+	}*/
+	
+	
+	public static<T extends Comparable<T>> void ordenar(T[] array) {
+		T aux;
+		
+		for (int i = 0 ; i < array.length-1 ; i++) {
+			for (int j = i+1 ; j < array.length ; j++) {
+				if (array[i].compareTo(array[j]) > 0) {
+					aux = array[i];
+					array[i] = array[j];
+					array[j] = aux;
+				}
+			}
+		}
+	}
 
 	public static void main(String[] args) {
 
 		//pruebasIniciales();
-		pruebasMatriz();
+		//pruebasMatriz();
+		
+		Integer[] numeros = {1,-3,44,2,6,7,0};
+		ordenar(numeros);
+		System.out.println(Arrays.toString(numeros));
+		
+		String[] nombres = {"Gema","Ana","Andres","Alberto","Raúl"};
+		ordenar(nombres);
+		System.out.println(Arrays.toString(nombres));
 
 	}
 
@@ -36,7 +76,16 @@ public class Principal {
 		
 		
 		Matriz<String> m2 = new Matriz<String>(6, 4, "hola");
+		char letra = 'A';
+		for (int i = 0 ; i < 6 ; i++) {
+			for (int j = 0 ; j < 4 ; j++, letra++) {
+				m2.set(i, j, letra+"");
+			}
+		}
+		
 		m2.print();
+		System.out.println();
+		m2.traspuesta().print();
 	}
 
 	private static void pruebasIniciales() {
