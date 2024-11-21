de7ab02f72ce99fed0e23382d18bb0cde36dc710
aldebarran22 <antonioespinherranz@hotmail.com>
2024-11-21 09:49:42 +0100

    9.49

---
+++ codigo_nov_2024/streams_lambda/src/es/curso/ficheros/GestorPedidos.java
@@ -0,0 +1,158 @@
+package es.curso.ficheros;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+import es.curso.ficheros.beans.Pedido;
+
+public class GestorPedidos {
+
+	private Map<String, List<Pedido>> mapaPaises;
+	private String path;
+	private String cabeceras;
+
+	public GestorPedidos(String path) {
+		this.path = path;
+		this.mapaPaises = new TreeMap<String, List<String>>();
+		this.cabeceras = null;
+
+		this.cargarMapa();
+	}
+	
+	public void borrarFicheros(String carpeta) {
+		File file;
+		File filePais;
+		String[] ficheros;
+		
+		file = new File(carpeta);
+		ficheros = file.list();
+		for (String fich : ficheros) {
+			filePais = new File(carpeta+fich);
+			filePais.delete();
+			
+			System.out.println(fich+" borrado");
+		}
+	}
+	
+	private void grabarFichero(String carpeta, String pais) {
+		FileOutputStream fichero;
+		String pathDestino;
+
+		fichero = null;
+		
+		try {
+			pathDestino = carpeta + pais + ".csv"; 
+			fichero = new FileOutputStream(pathDestino);
+			
+			// Grabar cabeceras:
+			fichero.write(cabeceras.getBytes());
+			fichero.write("\n".getBytes());
+			
+			for (String pedido : this.mapaPaises.get(pais)) {
+				fichero.write(pedido.getBytes());
+				fichero.write("\n".getBytes());
+			}
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			
+		} finally {
+			if (fichero != null) {
+				try {
+					fichero.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	public void generarFicheros(String carpeta, String...paises) {
+		
+		for (String pais : paises) {
+			this.grabarFichero(carpeta, pais);
+		}
+	}
+
+	public void generarFicheros(String carpeta) {
+	
+		for (String pais : this.mapaPaises.keySet()) {
+			this.grabarFichero(carpeta, pais);
+		}
+	}
+
+	public List<String> getNombrePaises() {
+		List<String> paises;
+
+		paises = new ArrayList<String>();
+		paises.addAll(mapaPaises.keySet());
+		
+		
+		return paises;
+	}
+
+	public int getNumPaises() {
+		return this.mapaPaises.size();
+	}
+
+	public int getNumPedidosPais(String pais) {
+		return this.mapaPaises.get(pais).size();
+	}
+
+	private void cargarMapa() {
+		Scanner scan = null;
+		String linea, pais;
+		String[] campos;
+		List<String> pedidos;
+
+		try {
+			scan = new Scanner(new File(path));
+
+			// Lectura del fichero: byte a byte!
+			while (scan.hasNextLine()) {
+				linea = scan.nextLine();
+				if (cabeceras == null) {
+					cabeceras = linea;
+					continue;
+				}
+
+				campos = linea.split(";");
+				pais = campos[campos.length - 1].replace(' ', '_');
+
+				// Colocar el pais en el mapa y añadir el pedido a su colección;
+				if (mapaPaises.containsKey(pais)) {
+					// Recuperamos la lista de pedidos de ese pais:
+					pedidos = mapaPaises.get(pais);
+
+				} else {
+					// Es la primera vez, hay que crear la colección
+					pedidos = new ArrayList<String>();
+				}
+
+				// Añadir el pedido a la colección:
+				pedidos.add(linea);
+
+				// Modificar el mapa:
+				mapaPaises.put(pais, pedidos);
+			}
+
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+
+		} finally {
+
+			if (scan != null) {
+				scan.close();
+			}
+		}
+
+	}
+
+}
---
+++ codigo_nov_2024/streams_lambda/src/es/curso/ficheros/beans/Pedido.java
@@ -0,0 +1,114 @@
+package es.curso.ficheros.beans;
+
+
+import java.io.Serializable;
+
+public class Pedido implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private int idPedido;
+	private String idCliente;
+	private int idEmpleado;
+	private int idEmpresaEnvio;
+	private double importe;
+	private String pais;
+
+	public Pedido() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Pedido(int idPedido, String idCliente, int idEmpleado, int idEmpresaEnvio, double importe, String pais) {
+		super();
+		this.idPedido = idPedido;
+		this.idCliente = idCliente;
+		this.idEmpleado = idEmpleado;
+		this.idEmpresaEnvio = idEmpresaEnvio;
+		this.importe = importe;
+		this.pais = pais;
+	}
+
+	public Pedido(String linea) {
+		this(linea, ";");
+	}
+
+	public Pedido(String linea, String sep) {
+		String datos[];
+
+		try {
+
+			datos = linea.split(sep);
+			this.idPedido = Integer.parseInt(datos[0]);
+			this.idCliente = datos[1];
+			this.idEmpleado = Integer.parseInt(datos[2]);
+			this.idEmpresaEnvio = Integer.parseInt(datos[3]);
+			this.importe = Double.parseDouble(datos[4].replace(",", "."));
+			this.pais = datos[5];
+
+		} catch (Exception e) {
+			System.out.println(linea);			
+		}
+	}
+	
+	public String toCSV(String sep) {
+		return this.idPedido + sep + this.idCliente + sep + this.idEmpleado + 
+				sep + this.idEmpresaEnvio + sep + this.importe + sep +
+				this.pais;
+	}
+
+	public int getIdPedido() {
+		return idPedido;
+	}
+
+	public void setIdPedido(int idPedido) {
+		this.idPedido = idPedido;
+	}
+
+	public String getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(String idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public int getIdEmpleado() {
+		return idEmpleado;
+	}
+
+	public void setIdEmpleado(int idEmpleado) {
+		this.idEmpleado = idEmpleado;
+	}
+
+	public int getIdEmpresaEnvio() {
+		return idEmpresaEnvio;
+	}
+
+	public void setIdEmpresaEnvio(int idEmpresaEnvio) {
+		this.idEmpresaEnvio = idEmpresaEnvio;
+	}
+
+	public double getImporte() {
+		return importe;
+	}
+
+	public void setImporte(double importe) {
+		this.importe = importe;
+	}
+
+	public String getPais() {
+		return pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+
+	@Override
+	public String toString() {
+		return "Pedido [idPedido=" + idPedido + ", idCliente=" + idCliente + ", idEmpleado=" + idEmpleado
+				+ ", idEmpresaEnvio=" + idEmpresaEnvio + ", importe=" + importe + ", pais=" + pais + "]";
+	}
+
+}
